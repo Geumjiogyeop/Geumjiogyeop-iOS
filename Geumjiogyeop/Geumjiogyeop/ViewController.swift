ff69b8dd1ebcd5580ede570ed1139989e191d97c
sycuuui <102959791+sycuuui@users.noreply.github.com>
2023-08-02 15:31:23 +0900

    init: 프로젝트 생성

---
+++ Geumjiogyeop/Geumjiogyeop/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Geumjiogyeop
+//
+//  Created by 이서연 on 2023/08/02.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
