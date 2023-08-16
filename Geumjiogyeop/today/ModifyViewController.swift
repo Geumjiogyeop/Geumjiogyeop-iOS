3c3d7e36f8e25d63147dd0b4189db9de34e8bc8e
sycuuui <102959791+sycuuui@users.noreply.github.com>
2023-08-16 22:18:21 +0900

    수정

--- Geumjiogyeop/today/ModifyViewController.swift
+++ Geumjiogyeop/today/ModifyViewController.swift
@@ -9,7 +9,7 @@ import UIKit
 import Alamofire
 import SwiftyJSON
 
-class ModifyViewController: UIViewController {
+class ModifyViewController: UIViewController, UITextFieldDelegate,UITextViewDelegate {
 
     @IBOutlet weak var imageView: UIImageView!
     @IBOutlet weak var contentTextView: UITextView!
@@ -24,6 +24,7 @@ class ModifyViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        navigationController?.delegate = self
         print(postID!)
         self.imageView.image = image
         self.contentTextView.text = content
@@ -40,31 +41,46 @@ class ModifyViewController: UIViewController {
         self.imagePicker.sourceType = .photoLibrary //앨범에서 가졍오는 코드
         self.imagePicker.allowsEditing = true
         self.imagePicker.delegate = self
+        
+        self.titleTextField.delegate = self
+        self.contentTextView.delegate = self
     }
     
+    func textField(_ textField: UITextField, shouldChangeCharactersIn range: NSRange, replacementString string: String) -> Bool {
+            // 텍스트 필드의 텍스트가 변경될 때 호출됩니다.
+            // 변경된 텍스트를 사용하여 원하는 처리를 수행할 수 있습니다.
+            if textField == titleTextField {
+                // titleTextField의 변경 처리
+                let updatedText = (textField.text as NSString?)?.replacingCharacters(in: range, with: string) ?? ""
+                titleTextField.text = updatedText
+                return true
+            }
+            return true
+        }
+    
     @objc func saveButtonTapped() {
-        guard let title = titleTextField.text, !title.isEmpty,
-              let content = contentTextView.text, !content.isEmpty,
+        guard let title = self.titleTextField.text,
+              let content = self.contentTextView.text,
               let image = imageView.image,
               let postID = self.postID else {
             print("Title, content, and image must not be empty")
             return
         }
-        
+
         let parameters: [String: Any] = [
-            "title": title,
-            "content": content,
+            "title": title,     // 수정된 title 사용
+            "content": content, // 수정된 content 사용
             // 기타 수정할 필드들 추가
         ]
-        
+
         let url = "http://175.45.194.93/today/\(postID)/"
-        
+
         if let imageData = image.jpegData(compressionQuality: 0.5) {
             AF.upload(
                 multipartFormData: { multipartFormData in
                     // 이미지 데이터를 파라미터에 추가
                     multipartFormData.append(imageData, withName: "image", fileName: "image.jpg", mimeType: "image/jpeg")
-                    
+
                     // 기타 파라미터들을 추가
                     for (key, value) in parameters {
                         if let data = "\(value)".data(using: .utf8) {
@@ -81,19 +97,17 @@ class ModifyViewController: UIViewController {
                 case .success(let value):
                     print("Success: \(value)")
                     // 서버 응답을 처리하는 코드 추가
+                    self.showModificationAlert()
+                    DispatchQueue.main.asyncAfter(deadline: .now()+6) {
+                        self.navigationController?.popViewController(animated: true)
+                    }
                 case .failure(let error):
                     print("Error: \(error)")
                 }
             }
         }
-        
-        showModificationAlert()
-        DispatchQueue.main.asyncAfter(deadline: .now()+6) {
-            self.navigationController?.popViewController(animated: true)
-        }
-        
+    }
 
-        }
     
     func showModificationAlert() {
             let alert = UIAlertController(title: nil, message: "수정되었습니다.", preferredStyle: .alert)
@@ -113,6 +127,7 @@ class ModifyViewController: UIViewController {
        
 
     }
+    
 
 }
 extension ModifyViewController: UIImagePickerControllerDelegate, UINavigationControllerDelegate {
